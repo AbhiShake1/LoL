ca67e08b82f2b57e827b8f263243a2abb9f6d8d9
abhishake1 <abhi9848038283@gmail.com>
2024-11-08 21:54:27 +0545

    feat: only a

--- lol/init.lua
+++ lol/init.lua
@@ -4,21 +4,25 @@ function Hole(options)
 	local self = {
 		port = options and options.port or 6969,
 		accepted_requests = {},
+		has_started = false,
 	}
 
 	---@private
 	function self:accept(request, callback)
+		assert(not self.has_started, "Tried to request when hole is already taking requests.")
+
 		self.accepted_requests[request:lower()] = callback
 	end
 
 	function self:expect_requests()
+		assert(not self.has_started, "Tried to accept requests more than once. Hole will tear now!!!")
+
 		local server = require("luvit.http").createServer(function(req, res)
 			local request = req.url:match("^/([^/]+)")
 			if request then
 				request = request:lower()
 				if self.accepted_requests[request] then
 					self.accepted_requests[request](self)
-					res:finish()
 				else
 					res:statusCode(404)
 					res:finish("Unknown request: " .. request)
@@ -32,6 +36,8 @@ function Hole(options)
 		server:listen(self.port, function()
 			print("Hole expecting ***k on port " .. self.port)
 		end)
+
+		self.has_started = true
 	end
 
 	local hole = {
